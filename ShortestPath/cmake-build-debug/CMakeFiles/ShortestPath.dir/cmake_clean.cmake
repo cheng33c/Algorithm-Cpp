e8a0b1bea2d51a711cf2c16d4ff57ef1241b89ff
cheng33c <asglitc12@gmail.com>
2018-09-22 18:40:32 +0800

    shortestPath

---
+++ ShortestPath/cmake-build-debug/CMakeFiles/ShortestPath.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShortestPath.dir/main.cpp.o"
+  "ShortestPath.pdb"
+  "ShortestPath"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShortestPath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
