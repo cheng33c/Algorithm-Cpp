ff5f6076b59e439a7ad33fd4d67e7646b9492a3e
cheng33c <asglitc12@gmail.com>
2018-09-26 20:29:06 +0800

    heapify

--- MaxHeap/Heap.h
+++ MaxHeap/Heap.h
@@ -6,7 +6,7 @@
 #define MAXHEAP_HEAP_H
 
 #include <algorithm>
-#include <random
+#include <random>
 #include <cmath>
 #include <ctime>
 #include <string>
@@ -50,6 +50,17 @@ public:
     MaxHeap(int capacity1) : capacity(capacity1) {
         data = new Item[capacity + 1];
         count = 0;
+    }
+
+    MaxHeap(Item arr[], int n) {
+        data = new Item[n+1];
+        capacity = n;
+        for (int i = 0; i < n; i ++)
+            data[i+1] = arr[i];
+        count = n;
+
+        for (int i = count / 2; i >= 1; i --)
+            shiftDown(i);
     }
 
     ~MaxHeap() {
